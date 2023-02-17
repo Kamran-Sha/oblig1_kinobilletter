e1d1abf86851527a3f76a459e127dd38d3740a23
kamran sha <kamran-sha@hotmail.com>
2023-02-17 15:13:11 +0100

    Initial commit

---
+++ src/main/java/com/example/oblig1_bestiling_av_kinobilletter/Oblig1BestilingAvKinobilletterApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1_bestiling_av_kinobilletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1BestilingAvKinobilletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig1BestilingAvKinobilletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oblig1_bestiling_av_kinobilletter/Oblig1BestilingAvKinobilletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1_bestiling_av_kinobilletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1BestilingAvKinobilletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
